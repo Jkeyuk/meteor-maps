377f3ace94401652130ee49bba9a8e957286df0e
Jkeyuk <Jonkero2@gmail.com>
2020-02-28 17:48:58 -0500

    Navbar - update button styling and formatting

--- src/meteor-map/NavBar.js
+++ src/meteor-map/NavBar.js
@@ -1,37 +1,49 @@
-import React from 'react'
-import { makeStyles } from '@material-ui/core/styles';
-import Typography from '@material-ui/core/Typography';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Button from '@material-ui/core/Button';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Button from "@material-ui/core/Button";
 
 const useStyles = makeStyles(theme => ({
-    root: {
-        flexGrow: 1,
-    },
-    menuButton: {
-        marginRight: theme.spacing(2),
-    },
-    title: {
-        flexGrow: 1,
-    },
+  root: {
+    flexGrow: 1
+  },
+  menuButton: {
+    marginRight: theme.spacing(2)
+  },
+  title: {
+    flexGrow: 1
+  }
 }));
 
 export default function NavBar() {
-    const css = useStyles();
-    return (
-        <React.Fragment>
-            <div className={css.root}>
-                <AppBar position="fixed">
-                    <Toolbar>
-                        <Typography variant="h6" className={css.title}>
-                            Meteor Maps
-                        </Typography>
-                        <Button variant="contained" color="secondary" className={css.menuButton}>Table</Button>
-                        <Button variant="contained" color="secondary" >Map</Button>
-                    </Toolbar>
-                </AppBar>
-            </div>
-        </React.Fragment>
-    )
+  const css = useStyles();
+  return (
+    <React.Fragment>
+      <div className={css.root}>
+        <AppBar position="fixed">
+          <Toolbar>
+            <Typography variant="h6" className={css.title}>
+              Meteor Maps
+            </Typography>
+            <Button
+              variant="contained"
+              color="secondary"
+              className={css.menuButton}
+            >
+              Table
+            </Button>
+            <Button
+              variant="contained"
+              color="secondary"
+              className={css.menuButton}
+            >
+              Map
+            </Button>
+          </Toolbar>
+        </AppBar>
+      </div>
+    </React.Fragment>
+  );
 }
