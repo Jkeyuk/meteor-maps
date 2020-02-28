cbd9245ee414faebbe998333812ac7b9f6f0688d
Jkeyuk <Jonkero2@gmail.com>
2020-02-28 13:14:08 -0500

    initialize meteor maps component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import MeteorMap from './meteor-map/MeteorMap';
 
-ReactDOM.render(<div>hi</div>, document.getElementById('root'));
+ReactDOM.render(<MeteorMap/>, document.getElementById('root'));
 
---
+++ src/meteor-map/MeteorMap.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class MeteorMap extends Component {
+    render() {
+        return (
+            <div>
+                hi MeteorMap
+            </div>
+        )
+    }
+}
