92ee7c7f5c36c63e18bf268032f7cf882aa62f3d
Jkeyuk <Jonkero2@gmail.com>
2020-02-28 21:45:35 -0500

    initial setup for map

--- src/meteor-map/MeteorMap.js
+++ src/meteor-map/MeteorMap.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import NavBar from './NavBar'
 import MeteorTable from './MeteorTable'
+import MeteorMapMap from './MeteorMapMap'
 
 export default function MeteorMap() {
     return (
         <React.Fragment>
             <NavBar/>
+            <MeteorMapMap/>
             <MeteorTable/>
         </React.Fragment>
     )
---
+++ src/meteor-map/MeteorMapMap.js
@@ -0,0 +1,15 @@
+import React from "react";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Typography from "@material-ui/core/Typography";
+import Container from "@material-ui/core/Container";
+
+export default function MeteorMapMap() {
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      <Container>
+        <Typography variant="h6">Meteor Maps</Typography>
+      </Container>
+    </React.Fragment>
+  );
+}
