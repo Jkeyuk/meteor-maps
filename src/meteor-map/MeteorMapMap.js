9fd1ececa1bfaf0146f62a4c7d2283e2a1c81cf4
Jkeyuk <Jonkero2@gmail.com>
2020-02-29 08:25:52 -0500

    Add begining of map component

--- src/meteor-map/MeteorMapMap.js
+++ src/meteor-map/MeteorMapMap.js
@@ -1,15 +1,25 @@
 import React from "react";
-import CssBaseline from "@material-ui/core/CssBaseline";
-import Typography from "@material-ui/core/Typography";
-import Container from "@material-ui/core/Container";
+import { Map, Marker, Popup, TileLayer } from "react-leaflet";
 
 export default function MeteorMapMap() {
+  const position = [51.14967, -1.81];
   return (
     <React.Fragment>
-      <CssBaseline />
-      <Container>
-        <Typography variant="h6">Meteor Maps</Typography>
-      </Container>
+      <Map center={position} zoom={5} style={{ height: "400px" }}>
+        <TileLayer
+          url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        />
+        <Marker position={position}>
+          <Popup>
+            <span>
+              A pretty CSS3 popup.
+              <br />
+              Easily customizable.
+            </span>
+          </Popup>
+        </Marker>
+      </Map>
     </React.Fragment>
   );
 }
