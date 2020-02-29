febb0a211a35cce1f77e1bb73f29b202ee74210b
Jkeyuk <Jonkero2@gmail.com>
2020-02-29 09:32:47 -0500

    Map: create state and begin events

--- src/meteor-map/MeteorMapMap.js
+++ src/meteor-map/MeteorMapMap.js
@@ -1,25 +1,55 @@
 import React from "react";
 import { Map, Marker, Popup, TileLayer } from "react-leaflet";
 
-export default function MeteorMapMap() {
-  const position = [51.14967, -1.81];
-  return (
-    <React.Fragment>
-      <Map center={position} zoom={5} style={{ height: "400px" }}>
-        <TileLayer
-          url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"
-          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        />
-        <Marker position={position}>
-          <Popup>
-            <span>
-              A pretty CSS3 popup.
-              <br />
-              Easily customizable.
-            </span>
-          </Popup>
-        </Marker>
-      </Map>
-    </React.Fragment>
-  );
+export default class MeteorMapMap extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      latlng: {
+        lat: 51.505,
+        lng: -0.09
+      }
+    };
+  }
+
+  onClickSet = e => {
+    this.setState({
+      latlng: {
+        lat: 22.505,
+        lng: -0.09
+      }
+    });
+  };
+
+  onViewportChanged = viewport => {
+    this.setState({ latlng: viewport.center });
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        <Map
+          center={this.state.latlng}
+          zoom={5}
+          style={{ height: "400px" }}
+          onClick={this.onClickSet}
+          onViewportChanged={this.onViewportChanged}
+        >
+          <TileLayer
+            url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          />
+          <Marker position={this.state.latlng}>
+            <Popup>
+              <span>
+                A pretty CSS3 popup.
+                <br />
+                Easily customizable.
+              </span>
+            </Popup>
+          </Marker>
+        </Map>
+      </React.Fragment>
+    );
+  }
 }
