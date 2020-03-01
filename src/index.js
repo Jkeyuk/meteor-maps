d425f330a09cb833bce376b2b78f8e725f67ce26
Jkeyuk <Jonkero2@gmail.com>
2020-02-29 22:04:02 -0500

    fix unused import

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import MeteorMap from "./meteor-map/MeteorMap";
-import Box from "@material-ui/core/Box";
 
 ReactDOM.render(<MeteorMap />, document.getElementById("root"));
